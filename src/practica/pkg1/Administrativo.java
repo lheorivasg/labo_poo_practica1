1dba54e37062b3edb942d48c1903bd21d989df91
Kirigalla <Nuotatore260104!>
2024-11-03 13:55:43 -0600

    actualizacion 1 de Administrativo

--- src/practica/pkg1/Administrativo.java
+++ src/practica/pkg1/Administrativo.java
@@ -4,24 +4,71 @@
  * and open the template in the editor.
  */
 package practica.pkg1;
-
 import java.util.Random;
 
-public class Administrativo extends Persona{
+public class Administrativo extends Trabajador{
     private String fechaNacimiento;
-    
-    public Administrativo(String fechaNacimiento, String nombre, String apellidoPaterno, String apellidoMaterno) {
-        super(nombre, apellidoPaterno, apellidoMaterno);
+
+    public Administrativo(String fechaNacimiento, String numEconomico) {
+        super(numEconomico);
         this.fechaNacimiento = fechaNacimiento;
     }
 
-    public static int getRandom() {
-        int range = (9 - 0) + 1;
-        int random = (int) ((range * Math.random()) + 0);
-        return random;
+    public Administrativo(String fechaNacimiento, String numEconomico, String nombre, String apellidoPaterno, String apellidoMaterno) {
+        super(numEconomico, nombre, apellidoPaterno, apellidoMaterno);
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    
+
+
+    public String generarRFC(){
+        StringBuilder rfc = new StringBuilder();
+        
+        String apellidoPaterno = getApellidoPaterno();
+        String apellidoMaterno = getApellidoMaterno();
+        String nombre = getNombre();
+        
+        // Obtener las dos primeras letras del apellido paterno
+        if(apellidoPaterno.length() >= 2){
+            rfc.append(apellidoPaterno.substring(0,2).toUpperCase());
+        } else {
+            rfc.append(apellidoPaterno.toUpperCase());
+        }
+        
+        // Primera Letra del apellido Materno (o "X" si no se encuentra)
+        if(apellidoMaterno.length() >= 1){
+            rfc.append(apellidoMaterno.substring(0,1).toUpperCase());
+        }else{
+            rfc.append("X");
+        }
+        
+        // Primera letra del nombre
+        if (nombre.length() >= 1){
+            rfc.append(nombre.substring(0,1).toUpperCase());
+        }
+        
+        //Fecha de nacimiento en formato AAMMDD
+        String[] fecha = fechaNacimiento.split("-");
+        if(fecha.length == 3){
+            rfc.append(fecha[0].substring(2,4)); // Ano 
+            rfc.append(fecha[1]); // Mes
+            rfc.append(2);        // Dia
+        }
+        
+        
+        //Homoclave (tres caracteres aleatorios
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        for(int i = 0; i < 3; i++){
+            int indiceAleatorio = (int)(Math.random() * caracteres.length());
+            rfc.append(caracteres.charAt(indiceAleatorio));
+        }
+        
+        return rfc.toString();
     }
 
 
+    
     public String getFechaNacimiento() {
         return fechaNacimiento;
     }
@@ -32,10 +79,8 @@ public class Administrativo extends Persona{
 
     
 
-    private static char randomChar(int numeroAleatorio_11) {
-        Random r = new Random();
-        return (char) (r.nextInt(26) + 'A');
-    }
+    
+    
 //    int numeroAleatorio_1 = getRandom(65, 90);
 //    char a = randomChar(numeroAleatorio_1);
 //    char b = randomChar(numeroAleatorio_1);
