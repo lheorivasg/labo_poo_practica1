d7df2a1b91c757f82900371966aa3f3db1f84dc9
Alumno21 <Alumno21@PC21-063048>
2024-11-07 18:01:40 -0600

    comentarios

--- src/practica/pkg1/Alumno.java
+++ src/practica/pkg1/Alumno.java
@@ -7,38 +7,82 @@ public class Alumno extends Persona {
     private String trimestreIngreso;
     private String unidad;
 
+    
+    /**
+     * Contructor vacio
+     */    
     public Alumno() {//Construcotr vacio
     }
 
+    
+    /**
+     * Contructor parametrizado unicamente con los atributos trimestreIngresado y unidad
+     * 
+     * @param trimestreIngreso
+     * @param unidad 
+     */
     public Alumno(String trimestreIngreso, String unidad) { //Constructor Parametrizado
         this.trimestreIngreso = trimestreIngreso;
         this.unidad = unidad;
     }
 
+    
+    /**
+     * Contructor parametrizado con todos los atributos de la clase
+     * 
+     * @param trimestreIngreso
+     * @param unidad
+     * @param nombre
+     * @param apellidoPaterno
+     * @param apellidoMaterno 
+     */
     public Alumno(String trimestreIngreso, String unidad, String nombre, String apellidoPaterno, String apellidoMaterno) {
         super(nombre, apellidoPaterno, apellidoMaterno);
         this.trimestreIngreso = trimestreIngreso;
         this.unidad = unidad;
     }
 
+    
+    /**
+     * Metodo para obtener el trimestre de ingreso
+     * @return 
+     */
     public String getTrimestreIngreso() {
         return trimestreIngreso;
     }
 
+    
+    
+    /**
+     * Metodo para establecer el trimestre de ingreso
+     * @param trimestreIngreso 
+     */
     public void setTrimestreIngreso(String trimestreIngreso) {
         this.trimestreIngreso = trimestreIngreso;
     }
 
+    /**
+     * Metodo para obtener el parametro unidad
+     * @return 
+     */
     public String getUnidad() {
         return unidad;
     }
 
+    
+    /**
+     * Metodo para establecer el parametro unidad
+     * @param unidad 
+     */
     public void setUnidad(String unidad) {
         this.unidad = unidad;
     }
     
     
-    
+    /**
+     * Metodo para asociar el nombre de la unidad con el primer digito de la matricular
+     * @return primer digito de la matricula
+     */
     public String obtenerUnidad(){
         if(unidad=="Xochimilco"){
         return "1";
@@ -53,6 +97,12 @@ public class Alumno extends Persona {
         }else
             return "#";
     }
+    
+    
+    /**
+     * Meotodo para asociar el segundo digito de la matricula con el trimestre de ingreso
+     * @return segundo digito
+     */
     public String tri(){
         String tri;
         tri= trimestreIngreso.substring(3,4);
@@ -68,18 +118,33 @@ public class Alumno extends Persona {
             return "#";
     }
 
+    
+    /**
+     * Metodo para gener numeros aleatorios
+     * @param min
+     * @param max
+     * @return un numero aleatorio del 0 al 9
+     */
     public static int getRandom(int min, int max) {
 		int range = (max - min) + 1;
      		int random = (int) ((range * Math.random()) + min);
 		return random;
 	}
 
+    
+    /**
+     * El metodo que llama construye 
+     */
     public void generarMatricula(){
        String matricula= obtenerUnidad() +trimestreIngreso.substring(0, 2)+ tri()+getRandom(0, 2)+getRandom(0, 4)+getRandom(5, 9)+getRandom(0, 9)+getRandom(0, 9) +getRandom(0, 9); 
         
         System.out.println("Matricula:"+matricula); 
     }
 
+    /**
+     * Metodo que representa el estado de los atributos de un objeto
+     * @return 
+     */
     @Override
     public String toString() {
         return "Alumno: " + "trimestreIngreso=" + trimestreIngreso + ", unidad=" + unidad;
